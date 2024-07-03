c5238e4c8d258212b3d01c209ed5999eb7146b94
naima-tushin <naima39tushin@gmail.com>
2024-07-04 05:39:44 +0600

    Testimonial Section UI Modified

--- script.js
+++ script.js
@@ -1,16 +1,4 @@
-function toggleFAQ(element) {
-    const faqAnswer = element.nextElementSibling;
-    const arrow = element.querySelector('.arrow');
-    if (faqAnswer.classList.contains('visible')) {
-        faqAnswer.classList.remove('visible');
-        arrow.innerHTML = '&#9660;';
-    } else {
-        faqAnswer.classList.add('visible');
-        arrow.innerHTML = '&#9650;';
-    }
-}
-
-
+// Testimonial Starts
 let currentSlideIndex = 0;
 
 function currentSlide(n) {
@@ -23,7 +11,7 @@ function updateSlide() {
   const dots = document.querySelectorAll('.dot');
   const totalSlides = Math.ceil(cards.children.length / 3);
 
-  cards.style.transform = `translateX(-${currentSlideIndex * 100 / totalSlides}%)`;
+  cards.style.transform = `translateX(-${currentSlideIndex * 137 / totalSlides}%)`;
 
   dots.forEach(dot => dot.classList.remove('active'));
   dots[currentSlideIndex].classList.add('active');
@@ -33,16 +21,17 @@ document.addEventListener('DOMContentLoaded', () => {
   const dots = document.querySelectorAll('.dot');
   dots[currentSlideIndex].classList.add('active');
 });
-
-
-
-
-document.addEventListener('DOMContentLoaded', () => {
-    const hamburger = document.querySelector('.hamburger');
-    const navLinks = document.querySelector('.nav-links');
-
-    hamburger.addEventListener('click', () => {
-        navLinks.classList.toggle('active');
-    });
-});
-
+// Testimonial Ends
+// FAQ Section Starts
+function toggleFAQ(element) {
+    const faqAnswer = element.nextElementSibling;
+    const arrow = element.querySelector('.arrow');
+    if (faqAnswer.classList.contains('visible')) {
+        faqAnswer.classList.remove('visible');
+        arrow.innerHTML = '&#9660;';
+    } else {
+        faqAnswer.classList.add('visible');
+        arrow.innerHTML = '&#9650;';
+    }
+}
+// FAQ Section Ends
